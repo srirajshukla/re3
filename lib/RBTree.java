0b91aec1c104282ba1cab5e1861b1f7129366e8b
Gyanendra Shukla <srirajshukla@gmail.com>
2022-05-23 15:08:11 +0530

    feat: add rb node
    the rb node stores the price and a double linked list
    reference to all the orders at the price level.

---
+++ lib/RBTree.java
@@ -0,0 +1,50 @@
+
+/**
+ * RedBlack Tree Node
+ * This node will contain the price level and a Double Linked List
+ * reference to all the orders at that price level.
+ */
+class RedBlackNode<T extends Comparable<T>> {
+    /** Possible color for this node */
+    public static final int BLACK = 0;
+    /** Possible color for this node */
+    public static final int RED = 1;
+	// the key of each node
+	public T key;
+
+    // the reference to the double linked list
+    public DoubleLinkedList orders;
+
+    /** Parent of node */
+    RedBlackNode<T> parent;
+    /** Left child */
+    RedBlackNode<T> left;
+    /** Right child */
+    RedBlackNode<T> right;
+    // the number of elements to the left of each node
+    public int numLeft = 0;
+    // the number of elements to the right of each node
+    public int numRight = 0;
+    // the color of a node
+    public int color;
+
+    RedBlackNode(){
+        color = BLACK;
+        numLeft = 0;
+        numRight = 0;
+        parent = null;
+        left = null;
+        right = null;
+        orders = new DoubleLinkedList();
+    }
+
+	// Constructor which sets key to the argument.
+	RedBlackNode(T key){
+        this();
+        this.key = key;
+	}
+}
+
+public class RBTree {
+    
+}
