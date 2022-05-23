d1a4fd9b716510b26b03869c69fba50463dcf0a0
Rishabh Adiga <rishabh.adiga@gmail.com>
2022-05-23 16:26:52 +0530

    feat: added orderbook outline

---
+++ lib/OrderBook.java
@@ -0,0 +1,31 @@
+public class OrderBook {
+    RBTree tree;
+
+    public OrderBook() {    // Default constructor
+        tree = new RBTree();
+    }
+
+    public void addOrder(){
+
+    }
+
+    public void deleteOrder(){
+
+    }
+    
+    public void updateOrderVolume(){
+
+    }
+
+    public void updateOrderPrice(){
+
+    }
+
+    public getBuyOrders(){
+
+    }
+
+    public getSellOrders(){
+
+    }
+}
