e435fb2229025bde529414cc0afc7565351c3c7e
Gyanendra Shukla <srirajshukla@gmail.com>
2022-05-24 00:10:53 +0530

    feat: methods to add buy order and sell orders

--- lib/OrderBook.java
+++ lib/OrderBook.java
@@ -2,17 +2,30 @@ public class OrderBook {
     OrderTree<BuyOrder> buyTree;
     OrderTree<SellOrder> sellTree;
 
+    PriceNode<Integer, BuyOrder> bestBuy;
+    PriceNode<Integer, SellOrder> bestSell;
+
     public OrderBook() {    // Default constructor
         buyTree = new OrderTree<>();
         sellTree = new OrderTree<>();
+        bestBuy = buyTree.treeMaximum(buyTree.root);
+        bestSell = sellTree.treeMinimum(sellTree.root);
     }
 
     public void addOrder(BuyOrder order){
+        int key = (int)(order.price * 100);
+        PriceNode<Integer, BuyOrder> node = buyTree.insert(key);
+        node.orders.add(order);
 
+        bestBuy = buyTree.treeMaximum(buyTree.root);
     }
 
     public void addOrder(SellOrder order){
+        int key = (int)(order.price * 100);
+        PriceNode<Integer, SellOrder> node = sellTree.insert(key);
+        node.orders.add(order);
 
+        bestSell = sellTree.treeMinimum(sellTree.root);
     }
 
     public void deleteOrder(){
