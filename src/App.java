99af121c66f0cc9f7a2671d8928179aefdef0443
Rishabh Adiga <rishabh.adiga@gmail.com>
2022-05-24 18:50:52 +0530

    restore app.java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,48 @@
+import Utils.DataGenerator;
+import orderbook.BuyOrder;
+import orderbook.OrderBook;
+import orderbook.SellOrder;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.List;
+
 public class App {
-    
+    public static void main(String[] args) throws Exception {
+       OrderBook ob = new OrderBook();
+
+        UUID id1 = UUID.randomUUID();
+        UUID id2 = UUID.randomUUID();
+        UUID id3 = UUID.randomUUID();
+        System.out.println(id1);
+        System.out.println(id2);
+        System.out.println(id3);
+
+        ob.addOrder(new BuyOrder(UUID.randomUUID(), 10, 23.02, "GOOG", new Date()));
+        ob.addOrder(new BuyOrder(UUID.randomUUID(), 12, 22.02, "GOOG", new Date()));
+        ob.addOrder(new SellOrder(UUID.randomUUID(), 10, 23.02, "GOOG", new Date()));
+        ob.addOrder(new BuyOrder(id2, 2, 27.02, "GOOG", new Date()));
+        ob.addOrder(new BuyOrder(id1, 17, 23.02, "GOOG", new Date()));
+        ob.addOrder(new SellOrder(id3, 11, 22.02, "GOOG", new Date()));
+
+        System.out.println(ob);
+
+        ob.deleteOrder(id1);
+        ob.updateOrderVolume(id2, 5);
+        ob.updateOrderPrice(id3, 23.02);
+
+       DataGenerator dg = new DataGenerator();
+       dg.writeOrdersToFile("GOOG", 1000, 20, 0.25);
+
+        List<BuyOrder> orders = dg.generateBuyOrder("GOOG", 100, 20, 0.10);
+        for (BuyOrder order : orders){
+            ob.addOrder(order);
+        }
+
+        System.out.println(ob);
+
+        List<SellOrder> lst = ob.getSellOrders();
+        System.out.println(lst);
+    }
 }
