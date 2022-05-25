efd96a2cb8019eab00b61373ac236eb17a994a2c
Gyanendra Shukla <srirajshukla@gmail.com>
2022-05-25 20:33:18 +0530

    feat: add trade members

--- lib/matchingengine/Trade.java
+++ lib/matchingengine/Trade.java
@@ -1,5 +1,42 @@
 package matchingengine;
 
-public abstract class Trade {
-    
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class Trade {
+    UUID tradeId;
+    int buyerId;
+    int sellerId;
+    String symbol;
+    int volume;
+    double price;
+    Date timestamp;
+
+    public Trade(){
+        this.tradeId = UUID.randomUUID();
+        this.timestamp = new Date();
+    }
+    public Trade(int buyerId, int sellerId, String symbol, int volume, double price) {
+        this.tradeId = UUID.randomUUID();
+        this.buyerId = buyerId;
+        this.sellerId = sellerId;
+        this.symbol = symbol;
+        this.volume = volume;
+        this.price = price;
+        this.timestamp = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return "Trade{" +
+                "tradeId=" + tradeId +
+                ", buyerId=" + buyerId +
+                ", sellerId=" + sellerId +
+                ", symbol='" + symbol + '\'' +
+                ", volume=" + volume +
+                ", price=" + price +
+                ", timestamp=" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(timestamp) +
+                '}';
+    }
 }
